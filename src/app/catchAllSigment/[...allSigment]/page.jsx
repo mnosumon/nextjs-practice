ff651f48c244061246e953969b9cabb56f6df8ec
mnosumon <mdnuruddin378350@gmail.com>
2024-12-06 16:02:21 +0600

    catch all sigment

---
+++ src/app/catchAllSigment/[...allSigment]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = ({ params }) => {
+  return (
+    <div>
+      <h1>All route print = {params.allSigment}</h1>
+    </div>
+  );
+};
+
+export default page;
